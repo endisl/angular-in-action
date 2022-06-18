6e5260f785e35907915c90393d06b5a0bfc1174d
endisl <endiluamba@gmail.com>
2022-06-17 23:51:41 -0300

    stocks

---
+++ stocks/src/app/services/stocks.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+ 
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
+let service: string = 'https://angular2-in-action-api.herokuapp.com';
+
+export interface StockInterface {
+  symbol: string;
+  lastTradePriceOnly: number;
+  change: number;
+  changeInPercent: number;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StocksService {
+
+  constructor(private http: HttpClient) {}
+
+  get() {
+    return stocks.slice();
+  }
+
+  add(stock) {
+    stocks.push(stock);
+    return this.get();
+  }
+
+  remove(stock) {
+    stocks.splice(stocks.indexOf(stock), 1);
+    return this.get();
+  }
+
+  load(symbols) {
+    if (symbols) {
+      return this.http.get<Array<StockInterface>> (service + 
+        '/stocks/snapshot?symbols=' + symbols.join());
+    }
+  }
+}
