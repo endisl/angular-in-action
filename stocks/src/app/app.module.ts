9d7faf78418609158f8a930e7167d4a0146bcf65
endisl <endiluamba@gmail.com>
2022-06-20 03:38:43 -0300

    stocks

--- stocks/src/app/app.module.ts
+++ stocks/src/app/app.module.ts
@@ -7,17 +7,21 @@ import { AppComponent } from './app.component';
 import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [StocksService],
   bootstrap: [AppComponent]
