c8ffa1b4f82e66594d9a2f2507200627c86eae37
endisl <endiluamba@gmail.com>
2022-06-27 01:01:37 -0300

    portfolio

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
+import { CurrencyPipe } from '@angular/common';
+
+import { AppComponent } from './app.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { TickerComponent } from './ticker/ticker.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { AlertComponent } from './alert/alert.component';
+
+import { LocalStorageService } from './services/local-storage.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+        InvestmentsComponent,
+        TickerComponent,
+        StocksComponent,
+        AlertComponent,
+      ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        ClarityModule,
+  ],
+  providers: [
+        LocalStorageService,
+        CurrencyPipe
+       ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
