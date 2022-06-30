3000f7b2e71b0078a9669eac369f1f06d43d99f8
endisl <endiluamba@gmail.com>
2022-06-30 10:59:45 -0300

    community

---
+++ community/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
+import { ForumsModule } from './forums/forums.module';
+
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+import { UserService } from './services/user.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/forums', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    ChatListComponent,
+    LoginComponent,
+    NotFoundComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ClarityModule,
+    ForumsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    UserService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ community/src/app/forums/forums.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ClarityModule } from '@clr/angular';;
+
+import { ForumComponent } from './forum/forum.component';
+import { ForumsComponent } from './forums/forums.component';
+import { ThreadComponent } from './thread/thread.component';
+import { ThreadsComponent } from './threads/threads.component';
+
+import { ForumsService } from './services/forums.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const forumsRoutes: Routes = [
+  { path: 'forums', component: ForumsComponent }
+];
+
+@NgModule({
+  declarations: [
+    ForumComponent,
+    ForumsComponent,
+    ThreadComponent,
+    ThreadsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ClarityModule,
+    RouterModule.forChild(forumsRoutes)
+  ],
+  providers: [
+    ForumsService
+  ]
+})
+export class ForumsModule { }
