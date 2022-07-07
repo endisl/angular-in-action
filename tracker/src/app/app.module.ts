cfc0590f065c9571355418f9480cb106f101239b
endisl <endiluamba@gmail.com>
2022-07-06 23:53:01 -0300

    tracker

--- tracker/src/app/app.module.ts
+++ tracker/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersService } from './services/users.service';
+import { FollowersComponent } from './components/followers/followers.component';
+import { FollowingComponent } from './components/following/following.component';
+
+let service: string = 'https://api.github.com/users/endisl/followers';
+
+export interface UsersInterface {
+
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FollowersComponent,
+    FollowingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule    
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ tracker/src/app/components/followers/followers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'followers',
+  templateUrl: './followers.component.html',
+  styleUrls: ['./followers.component.css']
+})
+export class FollowersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
